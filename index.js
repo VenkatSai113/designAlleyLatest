7bd063b55220d94be0e0eeae8ee8317096fa293b
venkat sai <venkatsai84639@gmail.com>
2023-12-21 14:17:31 +0530

    21Dec 23 commit

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
+const path12=require("path23")
+const sqlite4=require("sqlite")
 
 const initilizeDBAndServer = () => {
   app.listen(4000, () => {
     console.log("sever is running");
   });
+  app,listen()
 };
 initilizeDBAndServer();
